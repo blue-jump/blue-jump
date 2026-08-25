823b4f1aff72085d1425595ea4c82d285a322418
marshot9420 <marshot9420@gmail.com>
2026-08-25 15:53:46 +0900

    fix: format 적용

--- packages/design-system/src/web/index.ts
+++ packages/design-system/src/web/index.ts
@@ -1,8 +1,52 @@
-export type { ButtonProps, ButtonSize, ButtonVariant, FilterOptionProps, IconButtonProps, IconButtonSize, LinkButtonProps } from "./buttons";
+export type {
+  ButtonProps,
+  ButtonSize,
+  ButtonVariant,
+  FilterOptionProps,
+  IconButtonProps,
+  IconButtonSize,
+  LinkButtonProps,
+} from "./buttons";
 export { Button, FilterOption, IconButton, LinkButton } from "./buttons";
 export type { AvatarProps, AvatarSize, BadgeProps, CardProps, ImageFrameProps } from "./display";
 export { Avatar, Badge, Card, ImageFrame } from "./display";
-export type { ContainerProps, GridProps, SectionProps, SidebarContentProps, SidebarFooterProps, SidebarGroupLabelProps, SidebarGroupProps, SidebarHeaderProps, SidebarInsetProps, SidebarLayoutProps, SidebarNavItemProps, SidebarNavLinkProps, SidebarNavListProps, SidebarNavProps, SidebarProps, SidebarSeparatorProps, StackProps } from "./layout";
-export { Container, Grid, Section, Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarInset, SidebarLayout, SidebarNav, SidebarNavItem, SidebarNavLink, SidebarNavList, SidebarSeparator, Stack } from "./layout";
+export type {
+  ContainerProps,
+  GridProps,
+  SectionProps,
+  SidebarContentProps,
+  SidebarFooterProps,
+  SidebarGroupLabelProps,
+  SidebarGroupProps,
+  SidebarHeaderProps,
+  SidebarInsetProps,
+  SidebarLayoutProps,
+  SidebarNavItemProps,
+  SidebarNavLinkProps,
+  SidebarNavListProps,
+  SidebarNavProps,
+  SidebarProps,
+  SidebarSeparatorProps,
+  StackProps,
+} from "./layout";
+export {
+  Container,
+  Grid,
+  Section,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarLayout,
+  SidebarNav,
+  SidebarNavItem,
+  SidebarNavLink,
+  SidebarNavList,
+  SidebarSeparator,
+  Stack,
+} from "./layout";
 export type { DrawerProps } from "./overlay";
 export { Drawer } from "./overlay";
